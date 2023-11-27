7d82978b54876117661bd2084bce1b6950783618
wswoodruff <williamswoodruff@gmail.com>
2023-11-26 21:51:56 -0500

    Add linting stuff

---
+++ .eslintrc.cjs
@@ -0,0 +1,56 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime'
+    ],
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react'
+    ],
+    'settings': {
+        'react': {
+            'version': 'detect'
+        }
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'warn',
+            'always'
+        ],
+        'react/no-unknown-property': 'off',
+        'no-unused-vars': 'off'
+    }
+};
