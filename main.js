adee90bd6f0662c246194679dcd04dcc52cdc3c9
tripdragon <nickitdesign@gmail.com>
2023-12-23 21:36:56 -0500

    sdlfmsdlfsdsdfk sdf

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import { store as Store } from './alexandria/store';
 import Initializers from './alexandria/initializers/index';
 
 // import {fish} from './alexandria/tacos/narf.js';
-import {fish} from 'narf';
+// import {fish} from 'narf';
 
-fish();
+Initializers();
 
-Initializers(Store);
+// fish();
