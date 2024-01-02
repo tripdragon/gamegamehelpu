7b132dc77c8d8b0a8f03c560cc9b9d92ece265c9
tripdragon <nickitdesign@gmail.com>
2024-01-02 01:06:42 -0500

    cleanup main

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ import { fish } from 'narf';
 
 import { patchObject3D_CM } from 'alexandria/initializers/patchObject3D';
 
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { Plane, Vector3 } from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { Plane, Vector3 } from 'three';
 import { loadModelAsync } from 'alexandria/utils/loadModel';
 import { randomInRange } from 'alexandria/utils/stuff';
 
@@ -53,14 +53,14 @@ init();
 
 async function loadereee3894() {
 
-  var piece1 = await loadModelAsync("./models/trees_mwoie_1.glb");
+  var piece1 = await loadModelAsync('./models/trees_mwoie_1.glb');
   piece1.scale.setScalar(0.1);
   piece1.position.x = randomInRange(-4,4);
   piece1.position.z = randomInRange(-4,4);
   // #Code: nnnanananame38744 #
   // we need some name auto system here
   // temp name for now
-  piece1.name = "trees_mwoie_1";
+  piece1.name = 'trees_mwoie_1';
 
 
   debugger
@@ -72,49 +72,21 @@ async function loadereee3894() {
 
   // _b.geoPath1.addPiece(piece1, "corner", "bottomRight");
 
-                        // const floorPlane = new Plane(new Vector3(0,1,0), 0);
-                        //
-                        // const targetVecOfPlane = new Vector3();
-                        //
-                        // store.state.game.domElement.addEventListener("pointerdown", onPointerDown324);
-                        //
-                        // // findModel
-                        //
-                        // const foundItem1 = store.state.game.importedModels.findModelByName("trees_mwoie_1");
-                        //
-                        // function onPointerDown324(ev){
-                        //   // console.log(ev);
-                        //   let piece2 = foundItem1.clone();
-                        //   // let piece2 = piece1.clone();
-                        //   let _o = store.state.game;
-                        //   // _o.scene.add(piece2);
-                        //   _o.currentLevelMap.add(piece2);
-                        //   piece2.position.x = Math.random() * 4;
-                        //   piece2.position.z = Math.random() * 4;
-                        //   _o.planningBoard.add(piece2);
-                        //
-                        //   GetPositionOfRaycasterFromFloor({domElement:_o.renderer.domElement, ev:ev, camera: _o.camera, floorPlane:floorPlane, vector3in: targetVecOfPlane});
-                        //   // _o.onConsole.log("isdownBbb", "isdownBbb");
-                        //   piece2.position.copy(targetVecOfPlane);
-                        //
-                        // }
-
-
-  var piece2 = await loadModelAsync("./models/bench1.glb");
+  var piece2 = await loadModelAsync('./models/bench1.glb');
   // piece2.scale.setScalar(0.1);
   store.state.game.scene.add(piece2);
   store.state.game.importedModels.add(piece2);
   piece2.scale.setScalar(0.2);
-  piece2.name = "bench1";
+  piece2.name = 'bench1';
 
 
 
-  const piece3 = await loadModelAsync("./models/poly-cat.glb");
+  const piece3 = await loadModelAsync('./models/poly-cat.glb');
   // piece2.scale.setScalar(0.1);
   store.state.game.scene.add(piece3);
   store.state.game.importedModels.add(piece3);
   piece3.scale.setScalar(0.02);
-  piece3.name = "poly-cat";
+  piece3.name = 'poly-cat';
 }
 //
 //
